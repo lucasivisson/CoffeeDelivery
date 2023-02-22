ab56c3a718988859806637d81ed52ff8fb3e0bae
Lucas Ivisson <lucasivisson1@gmail.com>
2023-02-22 12:35:19 -0300

    ✨ introduce react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components'
-import { Header } from './components/Header';
+import ErrorPage from './errorPage';
+import { DefaultLayout } from './layouts/defaultLayout';
+import { Checkout } from './Pages/Checkout';
+import { Home } from './Pages/Home';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'checkout',
+        element: <Checkout/>
+      }
+    ]
+  },
+]);
 
 function App() {
   return (
    <ThemeProvider theme={defaultTheme}>
-    <Header/>
+    <RouterProvider router={router} />
     <GlobalStyle/>
    </ThemeProvider>
   )
