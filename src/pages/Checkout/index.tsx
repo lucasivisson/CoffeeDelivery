c8cf100f539a9f31fb6e232db2878beb41c28644
Lucas Ivisson <lucasivisson1@gmail.com>
2023-03-10 18:55:03 -0300

    ✨ introducing address and payment box on checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,67 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
 import { Sidebar } from './components/Sidebar'
+import {
+  CheckoutFooterContainer,
+  DefaultContainer,
+  OrderContainer,
+} from './styles'
 
 export function Checkout() {
   return (
-    <>
-      <body>body</body>
+    <DefaultContainer>
+      <OrderContainer>
+        <body>
+          <p>Complete seu pedido</p>
+
+          <span>
+            <MapPinLine size={18} />
+            Endereço de Entrega
+          </span>
+          <span>Informe o endereço onde deseja receber seu pedido</span>
+          <form action="">
+            <input type="number" name="cep" placeholder="CEP" />
+            <input type="text" name="street" placeholder="Rua" />
+            <input type="number" name="number" placeholder="Número" />
+            <input type="text" name="complement" placeholder="Complemento" />
+            <input type="text" name="neighborhood" placeholder="Bairro" />
+            <input type="text" name="city" placeholder="Cidade" />
+            <input type="text" name="uf" placeholder="UF" />
+          </form>
+        </body>
+        <CheckoutFooterContainer>
+          <p>
+            <CurrencyDollar size={18} />
+            Pagamento
+          </p>
+          <span>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </span>
+          <div>
+            <input type="radio" name="credit_card"></input>
+            <label htmlFor="credit_card">
+              <CreditCard size={18} />
+              Cartão de crédito
+            </label>
+            <input type="radio" name="debit_card"></input>
+            <label htmlFor="debit_card">
+              <Bank size={18} />
+              Cartão de débito
+            </label>
+            <input type="radio" name="money"></input>
+            <label htmlFor="money">
+              <Money size={18} />
+              Dinheiro
+            </label>
+          </div>
+        </CheckoutFooterContainer>
+      </OrderContainer>
       <Sidebar />
-    </>
+    </DefaultContainer>
   )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const DefaultContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 0 1rem;
+  flex-direction: row;
+  max-width: 72rem;
+  height: 100%;
+`
+
+export const OrderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const CheckoutFooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+`
